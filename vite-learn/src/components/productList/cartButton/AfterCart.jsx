a5f51d2867fe1f66e458064452a8f7320666ac74
Shaheed mon <sheheedk07@gmail.com>
2024-10-25 19:49:57 +0530

    initial commit

--- vite-learn/src/components/productList/cartButton/AfterCart.jsx
+++ vite-learn/src/components/productList/cartButton/AfterCart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './cartButtons.css'
 function AfterCart() {
   return (
-    <div className='afterCart'>
+    <div className='after-cart'>
       <button className='cart-count'>1</button>
       <button className="cart-couter-button">-</button>
     </div>
--- vite-learn/src/components/productList/cartButton/BeforeCart.jsx
+++ vite-learn/src/components/productList/cartButton/BeforeCart.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import './cartButtons.css'
+import React from 'react';
+import './cartButtons.css';
+
 function BeforeCart() {
   return (
-    <div className='before-cart'>
-      <button className='add-cart-button'>Add to cart</button>
+    <div className="before-cart">
+      <button className="add-cart-button">Add to cart</button>
     </div>
-  )
+  );
 }
 
-export default BeforeCart
+export default BeforeCart;
