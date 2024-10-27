ac4b0c4c770d777317735fc1074317b60f6cb0a5
Shaheed mon <sheheedk07@gmail.com>
2024-10-27 12:31:09 +0530

    commit

--- vite-learn/src/components/productList/ProductList.jsx
+++ vite-learn/src/components/productList/ProductList.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import './ProductList.css';
-import Products from '../../api/Products.json';
-import BeforeCart from './cartButton/BeforeCart';
-import AfterCart from './cartButton/AfterCart';
+import React, { useState } from "react";
+import "./ProductList.css";
+import Products from "../../api/Products.json";
+import BeforeCart from "./cartButton/BeforeCart";
+import AfterCart from "./cartButton/AfterCart";
 
 export default function ProductList() {
-  const [count,setCount]=useState(0)
-  const addToCart=()=>{
-    setCount(1)
-  }
-  console.log(count)
+  const [count, setCount] = useState(0);
+  const addToCart = () => {
+    setCount(1);
+  };
+  console.log(count);
   return (
     <section className="container">
       {Products?.map((product, key) => (
