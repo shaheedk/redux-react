67f5f9590706b37a886ad54a0ae43979cb88c281
Shaheed mon <sheheedk07@gmail.com>
2024-10-28 16:12:34 +0530

    initial commit

--- vite-learn/src/components/productList/cartButton/AfterCart.jsx
+++ vite-learn/src/components/productList/cartButton/AfterCart.jsx
@@ -4,6 +4,7 @@ function AfterCart() {
   return (
     <div className="after-cart">
       <button className="cart-counter-button" >+</button>
+      <div className="cart-count">1</div>
       <button className="cart-counter-button">- </button>
     </div>
   );
