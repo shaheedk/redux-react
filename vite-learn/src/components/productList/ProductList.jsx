ead43505b01f8b71878dda4f5c9fd8325239c1fc
Shaheed mon <sheheedk07@gmail.com>
2024-11-03 10:55:13 +0530

    commit

--- vite-learn/src/components/productList/ProductList.jsx
+++ vite-learn/src/components/productList/ProductList.jsx
@@ -4,33 +4,31 @@ import Products from "../../api/Products.json";
 import BeforeCart from "./cartButton/BeforeCart";
 import AfterCart from "./cartButton/AfterCart";
 import { useSelector } from "react-redux";
-
-
- function ProductList() {
-  const {cartCount}=useSelector((state)=>state.cart)
+function ProductList() {
+  const { cartCount } = useSelector((state) => state.cart);
 
   const [count, setCount] = useState(0);
-  console.log(cartCount,'===cart')
+  console.log(cartCount, "===cart");
   const addToCart = () => {
-     setCount(1);
+    setCount(1);
   };
 
-  useEffect(()=>{
-    console.log('render')
-    return ()=>{
-      console.log('Un Mount')
-    }
-  },[])
+  useEffect(() => {
+    console.log("render");
+    return () => {
+      console.log("Un Mount");
+    };
+  }, []);
   console.log(count);
 
   return (
     <section className="container">
-   {Products?.map((product, key) => (
+      {Products?.map((product, key) => (
         <div className="product-container" key={key}>
           <img src={product?.image} alt="" />
           <h3>{product?.title}</h3>
-       
-        {count >0 ?   <AfterCart />: <BeforeCart addToCarts={addToCart} />}
+
+          {count > 0 ? <AfterCart /> : <BeforeCart addToCarts={addToCart} />}
         </div>
       ))}
     </section>
