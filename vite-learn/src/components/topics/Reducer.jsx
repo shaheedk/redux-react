7c7d68d91e26bc0a2e3b080f605aa00cdda9a745
Shaheed mon <sheheedk07@gmail.com>
2024-12-25 22:01:15 +0530

    commit

--- vite-learn/src/components/topics/Reducer.jsx
+++ vite-learn/src/components/topics/Reducer.jsx
@@ -1,5 +1,18 @@
 import { useReducer } from "react"
 
+const initialState=0;
+const reducer=(state,action)=>{
+  switch(action){
+    case 'increment':
+      return state+1;
+      case 'decrement':
+        return state-1;
+        case 'reser':
+          return initialState;
+          default:
+            return state;
+  }
+}
 function Reducer() {
   return (
     <div>
